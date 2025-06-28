fcea41d68b4848284a3999ef6cbb05b7795d22db
Ayşenur Şirin <aysenursirin0@gmail.com>
2025-06-28 22:10:06 +0300

    created the bottom part of the sidebar

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -24,6 +24,18 @@ const Sidebar = () => {
         </div>
 
         <div className="bottom">
+            <div className="bottom-item recent-entry">
+                <img src={assets.question_icon} alt="" />
+                <p>Help</p>
+            </div>
+            <div className="bottom-item recent-entry">
+                <img src={assets.history_icon} alt="" />
+                <p>Activity</p>
+            </div>
+            <div className="bottom-item recent-entry">
+                <img src={assets.setting_icon} alt="" />
+                <p>Settings</p>
+            </div>
 
         </div>
 
